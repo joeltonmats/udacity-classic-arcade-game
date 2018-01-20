ab00b34b6945985a45d5cd3bd94a147e74740f78
joeltonmats <joeltonmatos@gmail.com>
2018-01-20 12:22:51 -0400

    added node server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+app.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+module.exports = app;
